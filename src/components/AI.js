5817bef725870a72a781a36ad6645a65b6cef500
Kunanon-Intaraphol <86783856+Kunanon-Intaraphol@users.noreply.github.com>
2022-08-23 13:54:56 +0700

    s
    s

--- src/components/AI.js
+++ src/components/AI.js
@@ -4,6 +4,7 @@ import { CircularProgressbar , buildStyles} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import "./aroi.css";
 import Logo from './icons/LOGO.png'
+import { Button, Card , Row, Col, Container , Navbar, Nav, Jumbotron } from 'react-bootstrap'
 
 const Read = () => {
     
@@ -26,14 +27,30 @@ const Read = () => {
 
     return (
         <div>
-            <div style={{backgroundColor:"#AFD0FF"}}>
+            {/* <div style={{backgroundColor:"#AFD0FF"}}>
             <center>
             <img src ={Logo}  style={{height:"60px"}}></img>
             </center>
-            </div>
+            </div> */}
+            
+            <Navbar className="NewsProof-Logo" style={{background :"#B2FFF1"}} expand="lg  ">
+                <Navbar.Brand>
+                <img src ={Logo} style={{height:"auto" ,width:"200px",marginLeft:"30%"}}></img>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <div className="me-auto"></div>
+                    <Nav>
+                        <Nav.Link href="https://www.antifakenewscenter.com" target="_blank"><h11 style={{marginLeft:"20px",marginRight:"20px"}}>Anti-Fake News Center Thailand</h11></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+            
+            
             <center>
-            <h2>โปรดใช้ดุลพินิจของท่านในการตัดสินใจ</h2>
-            <button style={{    
+            <h2 style={{marginTop:"20px"}}>โปรดใช้ดุลพินิจของท่านในการตัดสินใจ</h2>
+            {/* <button style={{    
             width: "250px",
             height: "60px",
             color: "#ebedf1",
@@ -47,7 +64,7 @@ const Read = () => {
             onClick={() => window.location.reload(false)}
             >
             กดเพื่อรับผลคำตอบ
-            </button>
+            </button> */}
             </center>  
 
             
@@ -115,7 +132,7 @@ const Frame = ({News , Sentence , FAI ,Status ,TAI ,INTFAI,onlyST,STonlyP}) => {
 
         return(
         <div className="div">
-            <h3>ผลลัพธ์</h3>
+            {/* <h3>ผลลัพธ์</h3>
 
             <p1>
             <center>
@@ -157,7 +174,102 @@ const Frame = ({News , Sentence , FAI ,Status ,TAI ,INTFAI,onlyST,STonlyP}) => {
                     }
                     })}
                 </table>
+            </div> */}
+
+
+
+
+<Container style={{ padding :"20px" ,marginTop:"20px"}}>
+    <center>
+      <Row>
+        <Col md={4} style={{marginTop:"20px"}}>
+          <Card style={{ padding:"20px"}}>
+            <Card.Body>
+              <center>
+              <Card.Img variant="top" src={Logo}></Card.Img>
+              <br />
+              <Card.Title>
+                <h1>ตรวจสอบข่าวสาร</h1>
+                <div style={{ width: 200, height: 200,marginTop:"30px"}}>
+                 <CircularProgressbar value={perpre}  text={`${perpre}%`} circleRatio={0.75} circleRatio={0.75} styles={buildStyles({
+                    rotation: 1 / 2 + 1 / 8,
+                    textColor:TCL,
+                    width: "150px", 
+                    height: "150px",
+                    trailColor: "#909090",
+                    pathColor:PCL,
+                    })}
+                />
+                </div>
+                <h4 style={{color:TCL,marginTop:"20px",marginBottom:"20px"}}>{predict}</h4>
+              </Card.Title>
+              </center>
+            </Card.Body>
+          </Card>
+        </Col>
+
+
+
+
+        <Col md={4} style={{marginTop:"20px"}}>
+          <Card style={{ padding:"5px"}}>
+            <Card.Body>
+              <br />
+              <Card.Title>
+                <h5>เนื้อหาข่าว</h5>
+                <textarea value={News} style={{ height:"383px",width:"100%",textAlign:"center"}}></textarea>
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        
+
+
+        <Col md={4} style={{marginTop:"20px"}} >
+          <Card style={{ padding:"2px",border:"0px"}}>
+            <Card.Body>
+            <div className="AppTable">
+                
+                    
+                    {data.map((val, key) => {
+                    if(fai>TAI){
+                        return (
+                            <table style={{border:"1px"}}> 
+                            <tr style={{border: "1px solid #3F89F3"}}>
+                                <th>ประโยคที่มีความคล้ายที่จะเป็นข่าวปลอม</th>
+                                <th style={{borderLeft: "1px solid #3F89F3",textAlign:"center"}}>%</th>
+                            </tr>
+                            
+
+                            <tr key={key}>
+                                <td style={{borderBottom: "1px solid #3F89F3"}}>{val.gender}</td>
+                                <td style={{borderBottom: "1px solid #3F89F3", borderLeft: "1px solid #3F89F3"}}>{val.name}</td>
+                            </tr>
+                            </table>
+                        )
+                    }
+                    })}
+                    
+                
             </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      </center>
+    </Container>
+
+
+
+
+
+
+
+
+
+
+
             
         </div>
         );
